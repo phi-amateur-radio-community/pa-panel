8c94cdbd2b2fa04a46409c4bb94a8147fcf37b49
rangeset <Rangeset24@outlook.com>
2026-05-21 00:58:19 +0800

    fix(core): public main

--- core/src/main/java/org/phiarc/panel/Main.java
+++ core/src/main/java/org/phiarc/panel/Main.java
@@ -1,7 +1,7 @@
 package org.phiarc.panel;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
