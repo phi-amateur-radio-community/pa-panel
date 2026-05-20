1ef6ca7e802ca9aacb6564b37976ed7ae30baee9
rangeset <Rangeset24@outlook.com>
2026-05-21 01:11:10 +0800

    feat(core): add logger for main

--- core/src/main/java/org/phiarc/panel/Main.java
+++ core/src/main/java/org/phiarc/panel/Main.java
@@ -1,7 +1,12 @@
 package org.phiarc.panel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        log.info("PA-Panel start");
     }
 }
